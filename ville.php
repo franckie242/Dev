e01781cc8768707b75e30014520675077727ea0e
franckie242 <franckie242@icloud.com>
2017-05-06 20:01:40 +0200

    Update ville.php

--- ville.php
+++ ville.php
@@ -9,20 +9,19 @@ $tab = array();
 for($i=1;$i<$nbTotalLignes;$i++){
     $ligneTab = explode(";", $ligne[$i]);
     // choisissons la colonne correspondant aux régions:
-        $tab[$ligneTab[18]] =$ligneTab[18];
+        $tab[$ligneTab[11]] =$ligneTab[11];
         
-        if ($i > 1 && !in_array($ligneTab[18], $tab)) {
-                array_push($tab, $ligneTab[18]); // On ajoute l'élément au tableau uniquement si celui-ci n'existe pas encore
+        if ($i > 1 && !in_array($ligneTab[11], $tab)) {
+                array_push($tab, $ligneTab[11]); // On ajoute l'élément au tableau uniquement si celui-ci n'existe pas encore
                 
         }
 }
 sort($tab, SORT_STRING); // On trie le tableau
-unset($tab[0]);
 
-echo "<form action=\"region.php\" method=\"post\">";
+echo "<form action=\"ville.php\" method=\"post\">";
     echo "<fieldset>";
-        echo "<label for=\"region\">Veuillez sélectionner ci-dessous la région que vous souhaitez trouver</label>";
-        echo "<select name=\"region\" id=\"region\">";
+        echo "<label for=\"ville\">Veuillez sélectionner  la ville que vous souhaitez trouver  </label>";
+        echo "<select name=\"ville\" id=\"ville\">";
 
 foreach($tab as $val){
     echo "<option>".$val."</option>";
@@ -32,30 +31,31 @@ echo "<input type=\"submit\" value=\"OK\">";
 echo "</fieldset>";
 echo "</form>";
 echo "<section>";
-$cpt = 0;
-if(isset($_POST['region'])){
+$cpt=0;
+if(isset($_POST['ville'])){
     for($j=1; $j<$nbTotalLignes;$j++){
         $ligneTab2 = explode(";", $ligne[$j]);
-        if(strstr($ligneTab2[18],$_POST['region'])){
-            $cpt++;
-            echo "<table>";
-                echo "<tr>";
-                    echo "<td>".$ligneTab2[2]."</td>";
-                    echo "<td>".$ligneTab2[3]."</td>";
-                    echo "<td>".$ligneTab2[5]."</td>";
-                    echo "<td>".$ligneTab2[9]."</td>";
-                    echo "<td>".$ligneTab2[10]."</td>";
-                    echo "<td>".$ligneTab2[11]."</td>";
-                    echo "<td>".$ligneTab2[14]."</td>";
-                    echo "<td>".$ligneTab2[16]."</td>";
-                    echo "<td>".$ligneTab2[17]."</td>";
-                    echo "<td>".$ligneTab2[18]."</td>";                 
-                echo "</tr>";
-            echo "</table>";
-        }
+            if(strstr($ligneTab2[11],$_POST['ville'])){
+                $cpt++;
+                echo "<table>";
+                    echo "<tr>";
+                        echo "<td>".$ligneTab2[2]."</td>";
+                        echo "<td>".$ligneTab2[3]."</td>";
+                        echo "<td>".$ligneTab2[5]."</td>";
+                        echo "<td>".$ligneTab2[9]."</td>";
+                        echo "<td>".$ligneTab2[10]."</td>";
+                        echo "<td>".$ligneTab2[11]."</td>";
+                        echo "<td>".$ligneTab2[14]."</td>";
+                        echo "<td>".$ligneTab2[16]."</td>";
+                        echo "<td>".$ligneTab2[17]."</td>";
+                        echo "<td>".$ligneTab2[18]."</td>";                 
+                    echo "</tr>";
+                echo "</table>";
+            }
     }
     echo $cpt." école(s) correspond(ent) à votre recherche";
 }
 echo "</section>";
+
 require_once("footer.inc.php");
 ?>
