1216094573bb5d7271aa04548aee6f17d2b24af1
Natval <nathan.valentin@laposte.net>
2017-05-17 20:57:43 +0200

    Update header.inc.php

--- header.inc.php
+++ header.inc.php
@@ -2,22 +2,24 @@
  <html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-		<title>Page d'accueil</title>
-		<link href="../style.css" rel="stylesheet" type="text/css">
+		<title>Cherch'école</title>
+		<link href="style/style.css" rel="stylesheet" type="text/css">
 	</head>
 	
 	<body>
 		<header>
 			<h1>Cherch'ecole</h1>
+				<h2>Tu cherches et... tu colles</h2>
 			<nav>
 				<ul class="acc">
-					<li class="acc-item"><a href="../index.php">Accueil</a></li>
-					<li class="acc-item"><a href="../ville.php">Par ville</a></li>
-					<li class="acc-item"><a href="../region.php">Par région</a></li>
-					<li class="acc-item"><a href="../academie.php">Par académie</a></li>
-					<li class="acc-item"><a href="../etablissement.php">Par type</a></li>
-					<li class="acc-item"><a href="../advanced.php">Recherche avancée</a></li>
-					<li class="acc-item"><a href="../ecoles.php">Toutes les écoles</a></li>
+					<li class="acc-item"><a href="index.php">Accueil</a></li>
+					<li class="acc-item"><a href="ville.php">Rechercher Par ville</a></li>
+					<li class="acc-item"><a href="region.php">Rechercher Par région</a></li>
+					<li class="acc-item"><a href="academie.php">Rechercher Par académie</a></li>
+					<li class="acc-item"><a href="etablissement.php">Rechercher Par type</a></li>
+					<li class="acc-item"><a href="advanced.php">Recherche avancée</a></li>
+					<li class="acc-item"><a href="perso.php">Recherche personnalisée</a></li>
+					
 				</ul>				
 			</nav>
 		</header>
